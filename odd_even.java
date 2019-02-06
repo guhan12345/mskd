4dbe207c1ecd42a2ffdac382ed60eb10271e090e
guhan12345 <47376281+guhan12345@users.noreply.github.com>
2019-02-06 15:57:57 +0530

    Update odd_even.java

--- odd_even.java
+++ odd_even.java
@@ -2,11 +2,11 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-	int i;
+	int g;
 	Scanner s=new Scanner(System.in);
 	System.out.println("enter the number");
-	i=s.nextInt();
-	if(i%2==0)
+	g=s.nextInt();
+	if(g%2==0)
 	{
 	    System.out.println("the given number is even");
 	}
