9f8450ca7fcd8400e31745bf48f28d5c6934e8ac
guhan12345 <47376281+guhan12345@users.noreply.github.com>
2019-02-06 15:52:09 +0530

    Create odd_even.java

---
+++ odd_even.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	int i;
+	Scanner s=new Scanner(System.in);
+	System.out.println("enter the number");
+	i=s.nextInt();
+	if(i%2==0)
+	{
+	    System.out.println("the given number is even");
+	}
+	else
+	{
+	    System.out.println("the given number is odd");
+	}
+	}
+}
