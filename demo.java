4a6f72dcac78f10c4e6e66f94414b54351b69350
guhan12345 <47376281+guhan12345@users.noreply.github.com>
2019-02-06 14:08:07 +0530

    Create  demo.java

---
+++ demo.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class Demo
+{
+    public static void main(String[] args) 
+    {
+        int i=3;
+        if(i>0)
+        {
+            System.out.println("Positive");
+        }
+        else if(i<0)
+        {
+            System.out.println("Negative");
+        }
+        else
+        {
+            System.out.println(" is neither positive nor negative");
+        }
+    }
+}
